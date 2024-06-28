107fc9560c1be11615d5e40cfd0c4de21ab3e30a
GencoMattia <gencomattia@hotmail.it>
2024-06-28 16:23:09 +0200

    possibly implemented session if login successful

--- src/login.php
+++ src/login.php
@@ -1,5 +1,27 @@
 <?php 
+include_once __DIR__ . "/utilities/array.php";
 
+session_start();
+
+$username = isset($_POST["username"]) ? $_POST["username"] : "null";
+
+$password = isset($_POST["password"]) ? $_POST["password"] : "null";
+
+$isUserFound = false;
+
+foreach($users as $key => $user) {
+    if( $username === $user["username"] && $password === $user["password"] ) {
+        $isUserFound = true;
+        echo "Login Successful";
+        break;
+    }
+};
+
+if ($isUserFound) {
+    $_SESSION["username"] = $_POST["username"];
+} else {
+    echo "No user with these credentials found!"; 
+}
 
 ?>
 
@@ -15,9 +37,15 @@
         <section>
             <form action="" method="post">
                 <div class="username-input">
+                    <label for="username">
+                        Username:
+                    </label>
                     <input type="text" name="username" id="username">
                 </div>
                 <div class="password-input">
+                    <label for="password">
+                        Password:
+                    </label>
                     <input type="password" name="password" id="password">
                 </div>
                 <div class="button-container">
