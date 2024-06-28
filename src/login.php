fa234605758aab4476c344a47963f0b48e704fe2
GencoMattia <gencomattia@hotmail.it>
2024-06-28 15:32:03 +0200

    added login base html structure

---
+++ src/login.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <main>
+        <section>
+            <form action="" method="post">
+                <div class="username-input">
+                    <input type="text" name="username" id="username">
+                </div>
+                <div class="password-input">
+                    <input type="password" name="password" id="password">
+                </div>
+                <div class="button-container">
+                    <button type="submit">
+                        Log in
+                    </button>
+                </div>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
